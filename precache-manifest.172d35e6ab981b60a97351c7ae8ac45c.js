20173868ad6662430974e5fffe48e32eedf1ad2c
Tiffany Nguyen <tiffanyknnc@gmail.com>
2019-08-27 15:25:51 -0400

    Updates

---
+++ precache-manifest.172d35e6ab981b60a97351c7ae8ac45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823d8d57032d4a82f420b9c1be733f95",
+    "url": "/kfwlab/index.html"
+  },
+  {
+    "revision": "a96b3268c4da6437ed6c",
+    "url": "/kfwlab/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c314b8bcb496f03dfcf",
+    "url": "/kfwlab/static/js/2.95ea124f.chunk.js"
+  },
+  {
+    "revision": "a96b3268c4da6437ed6c",
+    "url": "/kfwlab/static/js/main.e689a144.chunk.js"
+  },
+  {
+    "revision": "42ac221b7588e11280d4",
+    "url": "/kfwlab/static/js/runtime~main.a4fe7523.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kfwlab/static/media/logo.5d5d9eef.svg"
+  }
+]);
