4cad825e0747b8ba20fa0b0f33123705220c8449
Tiffany Nguyen <tiffanyknnc@gmail.com>
2019-08-27 15:45:21 -0400

    Updates

---
+++ precache-manifest.302227367ce6b2fa2a0e435f8641c412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00cb37e017cd1b17a7f24fa0c5d7acbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c109c42c9c4ae7545cb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c314b8bcb496f03dfcf",
+    "url": "/static/js/2.95ea124f.chunk.js"
+  },
+  {
+    "revision": "0c109c42c9c4ae7545cb",
+    "url": "/static/js/main.b0a505e3.chunk.js"
+  },
+  {
+    "revision": "546b8f0fc424659de721",
+    "url": "/static/js/runtime~main.a50433e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
